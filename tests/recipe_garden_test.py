c65239eb1186ec1e5ec4ec98b7722769f07435a7
Joshua Hailman <jhailman@ccs.neu.edu>
2017-04-10 20:57:03 -0400

    Update test fixtures, merge test files because pytest

--- tests/recipe_garden_test.py
+++ tests/recipe_garden_test.py
@@ -10,8 +10,10 @@ from recipe_garden import recipe_garden
 def client(request):
     """Standard Flask client fixture, see minitwit's test example"""
     db_fd, recipe_garden.app.config['DB_PATH'] = tempfile.mkstemp()
+    recipe_garden.app.config['TESTING'] = True
     client = recipe_garden.app.test_client()
     with recipe_garden.app.app_context():
+        recipe_garden.get_db()
         recipe_garden.run_db_schema()
 
     def teardown():
@@ -21,3 +23,35 @@ def client(request):
 
     request.addfinalizer(teardown)
     return client
+
+
+# Users
+
+def test_register_and_login(client):
+    with recipe_garden.app.app_context():
+        registered = recipe_garden.User.register(
+            "Mrs. Example", "mrs-example@example.com", "mrs_example")
+        assert registered != None
+        logged_in = recipe_garden.User.login(
+            "mrs-example@example.com", "mrs_example")
+        assert logged_in.email == registered.email and logged_in.name == registered.name
+
+
+def test_login_bad_email(client):
+    with pytest.raises(Exception):
+        with recipe_garden.app.app_context():
+            registered = recipe_garden.User.register(
+                "mrs. example", "mrs-example@example.com", "mrs_example")
+            assert registered != None
+            recipe_garden.user.login(
+                "mr-example@example.com", "mrs_example")
+
+
+def test_login_bad_password(client):
+    with pytest.raises(Exception):
+        with recipe_garden.app.app_context():
+            registered = recipe_garden.User.register(
+                "mrs. example", "mrs-example@example.com", "mrs_example")
+            assert registered != None
+            recipe_garden.user.login(
+                "mrs-example@example.com", "mr_example")
