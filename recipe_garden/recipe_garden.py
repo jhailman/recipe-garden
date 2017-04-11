fbeaaee2c904b2eca2ef634e8a043a1ab8966d89
Joshua Hailman <jhailman@ccs.neu.edu>
2017-04-10 20:57:16 -0400

    Simplify db management code

--- recipe_garden/recipe_garden.py
+++ recipe_garden/recipe_garden.py
@@ -14,13 +14,12 @@ app.config.from_object(__name__) # load the values set above into config
 app.config.from_envvar('RECIPE_GARDEN_SETTINGS', silent=True) # Override with env var
 api = Api(app) # Create REST API
 
-def get_db(): # See flask tutorial `minitwit`
+def get_db():
     """Gets an application-context-specific DB connection"""
-    top = _app_ctx_stack.top
-    if not hasattr(top, 'db'):
-        top.db = sqlite3.connect(app.config['DB_PATH'])
-        top.db.row_factory = sqlite3.Row
-    return top.db
+    if not hasattr(g, 'db'):
+        setattr(g, 'db', sqlite3.connect(app.config['DB_PATH']))
+        g.db.row_factory = sqlite3.Row
+    return g.db
 
 
 # Import stuff from common after creating `app` and `get_db`
@@ -29,26 +28,18 @@ from .common.user import User
 # Set up database
 def run_db_schema():
     """Run the schema to initialize the database"""
-    app.logger.info("Initializing the database")
     db = get_db()
     with app.open_resource('schema.sql', mode='r') as schema:
-        db.executescript(schema.read())
+        db.cursor().executescript(schema.read())
     db.commit()
 
 @app.teardown_appcontext
 def shutdown_db(error):
     """Handler to close the DB at the end of application contexts"""
-    get_db().close()
+    if hasattr(g, 'db'):
+        g.db.close()
 
 @app.route('/')
 def main_page():
     user = User.get_by_id(1)
     return render_template("home.html.j2", user=user)
-
-if __name__ == "recipe_garden.recipe_garden":
-    # try:
-    #     run_db_schema()
-    # except Exception as err:
-    #     # do nothing
-    #     pass
-    pass
