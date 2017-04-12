b3bb65032cae6d23e291f2abb1ae9d7743778464
Eliza Pedersen <Eliza Pedersen>
2017-04-12 13:15:06 -0400

    changed all id values to autoincrement, fixed a bug for primary key of shopping list and added a potato soup recipe with ingredients and directions

--- recipe_garden/schema.sql
+++ recipe_garden/schema.sql
@@ -9,7 +9,7 @@ USE recipe_schema;
 DROP TABLE IF EXISTS user;
 CREATE TABLE user
 (
-  id           INT(11)      NOT NULL,
+  id           INTEGER AUTO_INCREMENT,
   name	       VARCHAR(45) 	NOT NULL,
   password	   VARCHAR(93)	NOT NULL,
   email        VARCHAR(128) NOT NULL,
@@ -26,9 +26,9 @@ CREATE TABLE ingredient
 DROP TABLE IF EXISTS recipe;
 CREATE TABLE recipe
 (
-  id			    INT(11)		NOT NULL,
-  rname		  	VARCHAR(45) NOT NULL,
-  author_id		INT(11),
+  id			    INTEGER AUTO_INCREMENT,
+  name		  	VARCHAR(45) NOT NULL,
+  author_id		INT(11) NOT NULL,
   created     TIMESTAMP DEFAULT now() ON UPDATE now(),
   PRIMARY KEY (id),
   FOREIGN KEY (author_id) REFERENCES user(id)
@@ -37,7 +37,7 @@ CREATE TABLE recipe
 DROP TABLE IF EXISTS direction;
 CREATE TABLE direction
 (
-  id            INT(11)		NOT NULL,
+  id            INTEGER AUTO_INCREMENT,
   recipe_id		  INT(11)		NOT NULL,
   description   TEXT		  NOT NULL,
   ordernum      INT(11),
@@ -48,7 +48,7 @@ CREATE TABLE direction
 DROP TABLE IF EXISTS recipe_ingredient;
 CREATE TABLE recipe_ingredient
 (
-  id            INT(11)		NOT NULL,
+  id            INTEGER AUTO_INCREMENT,
   recipe_id     INT(11)		NOT NULL,
   ingredient  	VARCHAR(45)	NOT NULL,
   amount        VARCHAR(45),
@@ -60,9 +60,10 @@ CREATE TABLE recipe_ingredient
 DROP TABLE IF EXISTS shopping_list;
 CREATE TABLE shopping_list
 (
-  id              INT(11)   NOT NULL,
+  id              INTEGER AUTO_INCREMENT,
   user_id         INT(11)   NOT NULL,
   recipe_ingredient_id  INT(11) NOT NULL,
+  PRIMARY KEY (id),
   FOREIGN KEY (recipe_ingredient_id) REFERENCES recipe_ingredient(id),
   FOREIGN KEY (user_id) REFERENCES user(id)
 );
@@ -77,6 +78,38 @@ CREATE TABLE favorites
   FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
-INSERT INTO user (id, name, password, email) VALUES (
-    1, "footastic95", "footastic95", "foo@foo.com"
+INSERT INTO user (name, password, email) VALUES (
+    "footastic95", "footastic95", "foo@foo.com"
 );
+
+INSERT INTO ingredient VALUES
+("onion(s)"), ("potato(s)"), ("carrot(s)"), ("mushroom(s)"),
+("olive oil"), ("vegetable broth"), ("chard"), ("canned corn"),
+("garlic"), ("salt"), ("pepper"), ("dried parsley"),
+("dried oregano"), ("bay leaf");
+
+INSERT INTO recipe (name, author_id) VALUES
+("Potato Soup", 1);
+
+INSERT INTO recipe_ingredient (recipe_id, ingredient, amount) VALUES
+(1, "onion(s)", "1"), (1, "potato(s)", "5"), (1, "carrot(s)", "1"),
+(1, "mushroom(s)", "6 oz"), (1, "olive oil", "1/4 cup"),
+(1, "vegetable broth", "6 cups"), (1, "chard", "1 bunch"),
+(1, "garlic", "4 cloves"), (1, "salt", "1 tbsp"),
+(1, "pepper", "1 tsp"), (1, "dried parsley", "1 tbsp"),
+(1, "dried oregano", "1 tbsp"), (1, "bay leaf", "1");
+
+INSERT INTO recipe_ingredient (recipe_id, ingredient) VALUES
+(1, "canned corn");
+
+INSERT INTO direction (recipe_id, description, ordernum) VALUES
+(1, "Cut all veggies and mince the garlic", 1),
+(1, "Add the olive oil to a large pot on medium heat", 2),
+(1, "Sautee the onion until caramelized", 3),
+(1, "Add garlic and sautee for 1 minute more", 4),
+(1, "Add the rest of the ingredients to the pot except for chard and corn", 5),
+(1, "Bring the liquid to a boil and then turn heat down to simmer for 45 minutes", 6),
+(1, "Turn heat off and use an immersion blender to make the soup a smooth-thick consistency", 7),
+(1, "Heat some olive oil in a pan on medium heat", 8),
+(1, "Add chard to pan and sautee until wilted", 9),
+(1, "Assemble by adding soup to a bowl and topping with the canned corn and sauteed chard", 10);
