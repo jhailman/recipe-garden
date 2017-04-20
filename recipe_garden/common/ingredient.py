b68478ebed9f75ddeec3b35fed26249f6c10c808
Joshua Hailman <jhailman@ccs.neu.edu>
2017-04-19 21:29:57 -0400

    add new recipes

--- recipe_garden/common/ingredient.py
+++ recipe_garden/common/ingredient.py
@@ -2,6 +2,8 @@ from ..recipe_garden import get_db, app
 from sqlalchemy import text
 
 GET_BY_ID = text("SELECT * FROM recipe_ingredient WHERE id = :id")
+ENSURE_INGREDIENT = text("INSERT IGNORE INTO ingredient (name) VALUES (:ingredient)")
+ADD_INGREDIENT = text("INSERT INTO recipe_ingredient (recipe_id, ingredient, amount) VALUES (:recipe_id, :ingredient, :amount)")
 
 class RecipeIngredient:
     def __init__(self, row):
@@ -19,6 +21,12 @@ class RecipeIngredient:
     @staticmethod
     def get_by_id(id):
         return RecipeIngredient(get_db().execute(GET_BY_ID, id=id).fetchone())
+
+    @staticmethod
+    def add_to_recipe(recipe_id, amount, ingredient):
+        db = get_db()
+        db.execute(ENSURE_INGREDIENT, ingredient=ingredient)
+        db.execute(ADD_INGREDIENT, recipe_id=recipe_id, amount=amount, ingredient=ingredient)
 
     def get_recipe(self):
         from .recipe import Recipe
